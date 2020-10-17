29f92781aee90989305a5ba1adb9e85d09fcc22b
aki-it <mac@macnoMacBook-Air.local>
2020-10-18 01:12:50 +0900

    add 1-6 random set

--- Dice/Dice/ContentView.swift
+++ Dice/Dice/ContentView.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    let dice = ["1", "2", "3", "4", "5", "6"]
     var body: some View {
-        Text("Dice")
+        Text(dice.randomElement()!)
             .font(.system(size: 100))
             .fontWeight(.bold)
             .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
